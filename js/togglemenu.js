4562321ecb3afd83e04d210806093ea94f8c2026
Ryan Victor <ryanvictor.am@gmail.com>
2024-01-08 23:36:53 -0400

    feat(chatbox): Em construção!

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,6 +1,20 @@
     const botaoMenu = document.querySelector('.botao-menu');
     const iconeMenu = document.querySelector('.icon-menu');
+    const body = document.querySelector('body');
+    const sidebarAtivo = document.querySelector('.sidebar');
 
     botaoMenu.addEventListener('click', () => {
       iconeMenu.classList.toggle('ativo');
     });
+
+    botaoMenu.addEventListener('click', () => {
+      body.classList.toggle('ativo');
+    });
+
+    botaoMenu.addEventListener('click', () => {
+      sidebarAtivo.classList.toggle('ativo');
+    });
+
+
+
+    
