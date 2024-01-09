706af6f7932f4665ce68b0b033320146373b378a
Ryan Victor <ryanvictor.am@gmail.com>
2024-01-09 10:38:08 -0400

    feat(tooglemenu): Adicionar função togglefunctions

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,20 +1,31 @@
-    const botaoMenu = document.querySelector('.botao-menu');
-    const iconeMenu = document.querySelector('.icon-menu');
-    const body = document.querySelector('body');
-    const sidebarAtivo = document.querySelector('.sidebar');
+const botaoMenu = document.querySelector(".botao-menu");
+const iconeMenu = document.querySelector(".icon-menu");
+const body = document.querySelector("body");
+const sidebarAtivo = document.querySelector(".sidebar");
 
-    botaoMenu.addEventListener('click', () => {
-      iconeMenu.classList.toggle('ativo');
-    });
+botaoMenu.addEventListener("click", () => {
+  iconeMenu.classList.toggle("ativo");
+});
 
-    botaoMenu.addEventListener('click', () => {
-      body.classList.toggle('ativo');
-    });
+botaoMenu.addEventListener("click", () => {
+  body.classList.toggle("ativo");
+});
 
-    botaoMenu.addEventListener('click', () => {
-      sidebarAtivo.classList.toggle('ativo');
-    });
+botaoMenu.addEventListener("click", () => {
+  sidebarAtivo.classList.toggle("ativo");
+});
 
+// toggleFunctions
+const toggleFunctions = document.querySelector('.toggle-functions');
+const toggleBack = document.querySelector('.toggle-back')
+const functions = document.querySelector('.chat-functions');
 
+toggleFunctions.addEventListener('click', () => {
+  functions.classList.toggle('ativo');
+  toggleBack.classList.toggle('ativo');
+})
 
-    
+toggleBack.addEventListener('click', () => {
+  functions.classList.remove('ativo');
+  toggleBack.classList.remove('ativo');
+})
