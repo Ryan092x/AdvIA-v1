8bfbe585dc8ef3e87927d31fcfd6e800f34229b9
Ryan Victor <ryanvictor.am@gmail.com>
2024-01-09 19:38:14 -0400

    feat(responsive): Em contrução

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -5,27 +5,33 @@ const sidebarAtivo = document.querySelector(".sidebar");
 
 botaoMenu.addEventListener("click", () => {
   iconeMenu.classList.toggle("ativo");
-});
-
-botaoMenu.addEventListener("click", () => {
   body.classList.toggle("ativo");
-});
-
-botaoMenu.addEventListener("click", () => {
   sidebarAtivo.classList.toggle("ativo");
 });
 
 // toggleFunctions
-const toggleFunctions = document.querySelector('.toggle-functions');
-const toggleBack = document.querySelector('.toggle-back')
-const functions = document.querySelector('.chat-functions');
+const toggleFunctions = document.querySelector(".toggle-functions");
+const toggleBack = document.querySelector(".toggle-back");
+const functions = document.querySelector(".chat-functions");
+let larguraDaTela = window.innerWidth;
 
-toggleFunctions.addEventListener('click', () => {
-  functions.classList.toggle('ativo');
-  toggleBack.classList.toggle('ativo');
-})
+toggleFunctions.addEventListener("click", () => {
+  if (larguraDaTela < 1200) {
+    functions.classList.toggle("ativo");
+    toggleBack.classList.toggle("ativo");
+  }
+  if (larguraDaTela > 1200) {
+    functions.classList.toggle("ativo");
+    toggleBack.classList.toggle("ativo");
+  }
+});
 
-toggleBack.addEventListener('click', () => {
-  functions.classList.remove('ativo');
-  toggleBack.classList.remove('ativo');
-})
+toggleBack.addEventListener("click", () => {
+  if (larguraDaTela < 1200) {
+    functions.classList.toggle("ativo");
+    toggleBack.classList.toggle("ativo");
+  } else {
+    functions.classList.toggle("ativo");
+    toggleBack.classList.toggle("ativo");
+  }
+});
